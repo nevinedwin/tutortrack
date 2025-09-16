66278a0a83f83ba8d32f8c8c3d2ef17e5422cc2d
nevin.e <nevin.e@inapp.com>
2025-09-16 23:18:28 +0530

    inital commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
+import withPWA from '@ducanh2912/next-pwa';
+
+const isDev = process.env.NODE_ENV === 'development';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: 'public',
+  register: true,
+  disable: isDev,
+  workboxOptions: {
+    disableDevLogs: true
+  }
+})(nextConfig);
